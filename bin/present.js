be54efb21a364a5d46d73e3b0df7f7e994927578
Nathan Herald <me@nathanherald.com>
2018-04-17 14:50:42 +0200

    Reads a slides.json in the current working directory

--- bin/present.js
+++ bin/present.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log("hello world")
+const path = require('path')
+const fs = require('fs')
+const { inspect } = require('util')
+
+const slidesPath = path.join(process.cwd(), 'slides.json')
+
+try {
+  fs.statSync(slidesPath)
+} catch (e) {
+  console.error('Cannot find a slides.json in the current directory')
+  process.exit(1)
+}
+
+const slidesJSON = fs.readFileSync(slidesPath)
+try {
+  const slides = JSON.parse(slidesJSON)
+  console.log(inspect(slides))
+} catch (e) {
+  console.error("Couldn't understand the slides.json file")
+  process.exit(1)
+}
